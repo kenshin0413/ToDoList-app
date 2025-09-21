dc42d8479a10fb48e004da0f4962e7e2467490a3
kenshin0413 <kenshin1234569@gmail.com>
2025-09-21 18:02:44 +0900

    初期

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by miyamotokenshin on R 7/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
